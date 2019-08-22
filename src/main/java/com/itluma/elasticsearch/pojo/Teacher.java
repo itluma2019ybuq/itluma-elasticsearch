0ab74fe5ef9502f0d778ac393dc665fe8c3ba7e7
feiniao_glb <feiniao_glb@itluma.com>
2019-08-22 17:23:33 +0800

    提交新的修改

---
+++ src/main/java/com/itluma/elasticsearch/pojo/Teacher.java
@@ -0,0 +1,13 @@
+package com.itluma.elasticsearch.pojo;
+
+import lombok.Data;
+
+/**
+ * @author ·ÉÄñ
+ * @date 2019/8/22 - 14:07
+ */
+@Data
+public class Teacher{
+    private Integer id;
+    private String name;
+}
