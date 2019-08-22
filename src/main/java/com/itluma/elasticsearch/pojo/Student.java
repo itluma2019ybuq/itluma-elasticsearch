6c813d8718cd42a98bd5efe58468377b57e25a94
feiniao_glb <feiniao_glb@itluma.com>
2019-08-22 14:08:27 +0800

    新的功能开发

---
+++ src/main/java/com/itluma/elasticsearch/pojo/Student.java
@@ -0,0 +1,13 @@
+package com.itluma.elasticsearch.pojo;
+
+import lombok.Data;
+
+/**
+ * @author 飞鸟
+ * @date 2019/8/22 - 14:07
+ */
+@Data
+public class Student {
+    private Integer id;
+    private String name;
+}
